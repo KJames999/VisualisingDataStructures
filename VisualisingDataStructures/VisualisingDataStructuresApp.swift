c86c4812d12e863a315c4ab2c17fee4b8713ddfa
Killian James <james.k@etoncollege.org.uk>
2022-03-03 11:55:23 +0000

    Initial Commit

---
+++ VisualisingDataStructures/VisualisingDataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataStructuresApp.swift
+//  VisualisingDataStructures
+//
+//  Created by James, Killian (NA) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
